f52d160aab5c8183e333996dc6ada2f672249ae8
Hassaan-Qaisar <106833785+Hassaan-Qaisar@users.noreply.github.com>
2023-08-27 01:58:26 +0500

    Update Bookings.jsx

--- client/src/pages/Bookings/Bookings.jsx
+++ client/src/pages/Bookings/Bookings.jsx
@@ -1,17 +1,30 @@
-import React, { useContext, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SearchBar } from "../../components/SearchBar/SearchBar";
 import { useProperties } from "../../hooks/useProperties";
 import { PuffLoader } from "react-spinners";
 import { PropertyCard } from "../../components/PropertyCard/PropertyCard";
 import "../Properties/Properties.css";
-import UserDetailContext from "../../context/UserDetailsContext";
+import { useAuth0 } from "@auth0/auth0-react";
+import { getAllBookings } from "../../utils/api";
+
 
 export const Bookings = () => {
-  const { data, isError, isLoading } = useProperties();
+  const { user } = useAuth0();
   const [filter, setFilter] = useState("");
-  const {
-    userDetails: { bookings },
-  } = useContext(UserDetailContext);
+  const [bookings, setBookings] = useState([]);
+  const { data, isError, isLoading } = useProperties(); // Assuming you have a custom hook for fetching properties
+
+  useEffect(() => {
+    if (user) {
+      getAllBookings(user.email)
+        .then((bookingsData) => {
+          setBookings(bookingsData);
+        })
+        .catch((error) => {
+          console.error("Error fetching bookings:", error);
+        });
+    }
+  }, [user]);
 
   if (isError) {
     return (
@@ -35,36 +48,28 @@ export const Bookings = () => {
     );
   }
 
-  //console.log(data)
-
   return (
     <div className="wrapper">
       <div className="flexColCenter paddings innerWidth properties-container">
         <SearchBar filter={filter} setFilter={setFilter} />
 
         <div className="paddings flexCenter properties">
-          {
-            // data.map((card, i) => (
-            //   <PropertyCard card={card} key={i} />
-            // ))
-
-            data
-              .filter((property) =>
-                bookings.map((booking) => booking.id).includes(property.id)
-              )
-
-              .filter(
-                (property) =>
-                  property.title.toLowerCase().includes(filter.toLowerCase()) ||
-                  property.city.toLowerCase().includes(filter.toLowerCase()) ||
-                  property.country.toLowerCase().includes(filter.toLowerCase())
-              )
-              .map((card, i) => (
-                <PropertyCard card={card} key={i} />
-              ))
-          }
+          {data
+            .filter((property) =>
+              bookings.map((booking) => booking.id).includes(property.id)
+            )
+            .filter(
+              (property) =>
+                property.title.toLowerCase().includes(filter.toLowerCase()) ||
+                property.city.toLowerCase().includes(filter.toLowerCase()) ||
+                property.country.toLowerCase().includes(filter.toLowerCase())
+            )
+            .map((card, i) => (
+              <PropertyCard card={card} key={i} />
+            ))}
         </div>
       </div>
     </div>
   );
 };
+
